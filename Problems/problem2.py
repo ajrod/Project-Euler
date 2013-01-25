adac200b08a42343997409a72b37b504dd24ad24
ajrod <alex.rodrigues@mail.utoronto.ca>
2013-01-25 01:54:53 -0500

    Added question credit to the source

--- Problems/problem2.py
+++ Problems/problem2.py
@@ -9,6 +9,8 @@ terms. By starting with 1 and 2, the first 10 terms will be:
 
 By considering the terms in the Fibonacci sequence whose values do not exceed 
 four million, find the sum of the even-valued terms.
+
+Question made by http://projecteuler.net/.
 '''
 ###############################################################################
 
--- Problems/problem3.py
+++ Problems/problem3.py
@@ -5,6 +5,8 @@ import math
 The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
+
+Question made by http://projecteuler.net/.
 '''
 ###############################################################################
 
